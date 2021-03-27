4b5330fea42a250abb1f7e33f420cf8601894c76
Alister Burt <alisterburt@gmail.com>
2021-03-27 18:25:47 +0000

    add README.md and remove Path.listdir()

---
+++ README.md
@@ -0,0 +1,19 @@
+# pathlibls
+
+Love Python's [pathlib](https://docs.python.org/3/library/pathlib.html)?  Think listing the contents of a directory should be easier? You've come to the right place!
+
+Say no to this:
+```python
+from pathlib import Path
+
+contents = [item for item in Path('/my/favourite/folder').iterdir()]
+contents = list(Path('/my/favourite/folder').glob('*'))
+```
+
+
+Say yes to `pathlibls`
+```python
+from pathlibls import Path
+
+contents = Path('/my/favourite/folder').ls()
+```
--- pathlibls/path.py
+++ pathlibls/path.py
@@ -2,14 +2,8 @@ from pathlib import Path as BasePath
 
 
 class Path(type(BasePath())):
-    def listdir(self):
-        """When the path points to a directory, return a list of path objects
-        of the directory contents
-        """
-        return [item for item in self.iterdir()]
-
     def ls(self):
         """When the path points to a directory, return a list of path objects
         of the directory contents
         """
-        return self.listdir()
+        return [item for item in self.iterdir()]
